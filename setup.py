ddbce063a3a126163dd96baf0ab8b5575604c227
bert.coerver <bertcoerver@users.noreply.github.com>
2024-03-28 15:02:17 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'wapordl',
-    version = '0.9',
+    version = '0.10.0',
     packages = find_packages(include = ['wapordl', 'wapordl.*']),
     include_package_data=True,
     python_requires='>=3.7',
     install_requires = [
         "requests",
-        "pandas>=2.1.0",
+        "pandas>=2.1.0,<3",
         "numpy",
-        "gdal>=3.4.0",
+        "gdal>=3.4.0,<4",
         "shapely>=2.0.0",
         "tqdm",
     ],
